9756c275fd6ab7eb6651071bea69f756263c1a01
Nishant Srivastava <nisrulz@gmail.com>
2016-09-23 14:23:11 +0530

    implemented static layout import feature

--- app/src/main/java/github/nisrulz/sample/learnkotlin/MainActivity.kt
+++ app/src/main/java/github/nisrulz/sample/learnkotlin/MainActivity.kt
@@ -3,10 +3,27 @@ package github.nisrulz.sample.learnkotlin
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
+// Get the references for all the views in the layout with a defined ID, via below import.
+// This is IMPORTANT to get reference of all views under the exact layout
+import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    /* Static Layout Import
+     Kotlin allows you to import all references to views from the layout with one import.
+
+     The TextView from layout is imported as a TextView instance with the name equal to the ID of the view,
+     with the name equal to the id in camelCase.
+
+     Note: 1. You don’t need to write semicolons at the end of the lines in Kotlin because they are optional.
+           2. When using fragments, make sure imported View references are used after the onCreateView() function call.
+              Import the layout in onCreateView() function and use the View references to setup the UI in onViewCreated().
+              The references won’t be assigned before the onCreateView() method has finished.
+     */
+    txt_helloworld.text = "Hello Kotlin World!"
   }
 }
