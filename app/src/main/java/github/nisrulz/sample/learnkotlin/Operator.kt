b277347a89167644f9263347b68cb5b2e50337fc
Nishant Srivastava <nisrulz@gmail.com>
2018-11-03 02:12:14 +0100

    🚧 code cleanup

--- app/src/main/java/github/nisrulz/sample/learnkotlin/Operator.kt
+++ app/src/main/java/github/nisrulz/sample/learnkotlin/Operator.kt
@@ -4,5 +4,4 @@ package github.nisrulz.sample.learnkotlin
   Note that you have to delegate to the default constructor of the extended class, which is similar to
   calling super() method in the constructor of a new class in Java.
  */
-class Operator(name1: String, name2: String) : User(name1, name2) {
-}
+class Operator(name1: String, name2: String) : User(name1, name2)
--- app/src/main/java/github/nisrulz/sample/learnkotlin/User.kt
+++ app/src/main/java/github/nisrulz/sample/learnkotlin/User.kt
@@ -29,27 +29,27 @@ package github.nisrulz.sample.learnkotlin
      like final classes in Java. So, in order to extend a class, it has to be declared as open or abstract
 */
 open class User(fName: String, lName: String) {
-  /*
-   When defining variables in Kotlin, there are two options:
-    1. Mutable variables, defined by var keyword.
-    2. Immutable variables, defined by val keyword.
+    /*
+     When defining variables in Kotlin, there are two options:
+      1. Mutable variables, defined by var keyword.
+      2. Immutable variables, defined by val keyword.
 
-   Note : 1. By default, properties are non-null types, meaning that they can’t accept null value.
-          2. By default, everything is of public scope. You don’t have to write public keyword again.
-             To define a variable to accept a null value, a question mark must be added after the type.
-          3. by default, all getter and setter are implicitly available you dont need to define unless you want a custom version for the same.
-   */
+     Note : 1. By default, properties are non-null types, meaning that they can’t accept null value.
+            2. By default, everything is of public scope. You don’t have to write public keyword again.
+               To define a variable to accept a null value, a question mark must be added after the type.
+            3. by default, all getter and setter are implicitly available you dont need to define unless you want a custom version for the same.
+     */
 
-  // Initializing properties using constructor variables
-  var firstName: String? = fName
-  var lastName: String? = lName
+    // Initializing properties using constructor variables
+    var firstName: String? = fName
+    var lastName: String? = lName
 
-  // Custom getter
-  val fullName: String? get() = firstName + " " + lastName
+    // Custom getter
+    val fullName: String? get() = "$firstName $lastName"
 
-  // Defining a member function in kotlin
-  // More info at https://kotlinlang.org/docs/reference/functions.html
-  fun sayHello(name: String): String {
-    return "Hello " + name
-  }
+    // Defining a member function in kotlin
+    // More info at https://kotlinlang.org/docs/reference/functions.html
+    fun sayHello(name: String): String {
+        return "Hello $name"
+    }
 }
